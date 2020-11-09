e0a1af97a5a45a2aad3a255933232bfc3137d32f
Afleabagby <arby70317@gmail.com>
2020-11-09 14:01:00 +0800

    丙級vue

---
+++ src/components/Top.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+//- Vue 的 img src 用 v-bind 時 ./ 代表 public 資料夾
+//- 沒有用 v-bind 的話會找 src
+#top
+    img(:src="'./images/logo.png'")
+</template>
+
+<script>
+export default {
+  name: 'Top'
+}
+</script>
